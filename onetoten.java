577a334800759712a052887fc33486b33cceb75b
Harsh0804 <61692637+Harsh0804@users.noreply.github.com>
2023-02-01 09:41:16 +0530

    xfh

---
+++ onetoten.java
@@ -0,0 +1,16 @@
+class onetoten
+{
+    public static void main(String args[])
+    {
+        int i ;
+        for(i=1;i<=10;i++)
+        {
+          System.out.println(i);  
+        }
+        while(i<11)
+        {
+          System.out.println(i); 
+          i++; 
+        }  
+    }
+}
